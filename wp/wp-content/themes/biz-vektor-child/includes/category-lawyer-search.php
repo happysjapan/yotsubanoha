d66aa84bc3db92812840765a4a5272a95fd6b7b3
julian-printemps <julian.printemps@gmail.com>
2016-02-04 16:43:00 +0900

    Search page lawyers

--- wp/wp-content/themes/biz-vektor-child/includes/category-lawyer-search.php
+++ wp/wp-content/themes/biz-vektor-child/includes/category-lawyer-search.php
@@ -2,29 +2,61 @@
   <h2>Lawyer</h2>
   <div class="inner">
 
+    <?php
+    $subcategories_parent = get_category_by_slug('47zenkoku');
+    $sub_args = array('child_of' => $subcategories_parent->term_id);
+    $subcategories = get_categories( $sub_args );
+
+    $exclude_ids = array();
+    $i=0;
+    foreach($subcategories as $subcategory) {
+      $exclude_ids[$i] = $subcategory->term_id;
+      $i++;
+    }
+
+    $args = array(
+      'exclude' => $exclude_ids
+    );
+    $categories = get_categories($args);
+
+    // echo '<pre>';
+    // var_dump($categories);
+    // echo '</pre>';
+
+    ?>
+
     <!-- [ #search form ] -->
-    <form method="get" id="searchform" action="<?php echo home_url(); ?>/info">
+    <form method="get" id="searchform" action="<?php echo home_url(); ?>/search">
 
       <input class="topSearch" type="search" placeholder="フリーワード" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s">
 
       <div class="select-box">
-        <label for ="searchSelect" class="search--form--label">お住いの地域をお選びください</label>
-        <select id="searchSelect" name="info_cat" class="search--form--select">
+        <label for ="searchSelect" class="search--form--label">お住いの地域をお選びください
+        <select id="searchSelect" name="category_name" class="search--form--select">
           <option value="" selected>Default</option>
           <?php
-            $categories = get_categories();
-
-            echo '<pre>';
-            var_dump($categories);
-            echo '</pre>';
-            foreach ($tax_terms as $tax_term) {
-              echo '<option value="'.$tax_term->slug.'">'.$tax_term->name.'</option>';
+            foreach ($categories as $category) {
+              echo '<option value="'.$category->slug.'">'.$category->name.'</option>';
             }
           ?>
         </select>
+        </label>
       </div>
 
-      <input type="hidden" name="post_type" value="info" />
+      <div class="select-box">
+        <label for ="searchSelect" class="search--form--label">Subcategories
+        <select id="searchSelect" name="subcategory_name" class="search--form--select">
+          <option value="" selected>Default</option>
+          <?php
+            foreach ($subcategories as $subcategory) {
+              echo '<option value="'.$subcategory->slug.'">'.$subcategory->name.'</option>';
+            }
+          ?>
+        </select>
+        </label>
+      </div>
+
+      <input type="hidden" name="post_type" value="lawyer" />
       <input class="btn" id="searchsubmit"  type="submit" value="検索する">
     </form>
     <!-- [ /#search form ] -->
--- wp/wp-content/themes/biz-vektor-child/page-lawyer.php
+++ wp/wp-content/themes/biz-vektor-child/page-lawyer.php
@@ -1,10 +1,50 @@
-
 <?php
 /*
   Template Name: Lawyer page
 */
 
-get_header(); ?>
+$post_search = $_GET['s'];
+$post_category_slug = $_GET['category_name'];
+$post_subcategory_slug = $_GET['subcategory_name'];
+
+if( isset($post_search)) {
+  if( isset($post_subcategory_slug) && $post_subcategory_slug != '' ) {
+    echo 'if 1';
+    $post_args = array(
+      's'             => $post_search,
+      'post_type'     => 'post',
+      'category_name' => $post_subcategory_slug
+    );
+  } else if( isset($post_category_slug) && $post_category_slug != '' ) {
+    echo 'if 2';
+    $post_args = array(
+      's'             => $post_search,
+      'post_type'     => 'post',
+      'category_name' => $post_category_slug
+    );
+  } else {
+    echo 'if 3';
+    $post_args = array(
+      's'             => $post_search,
+      'post_type'     => 'post'
+    );
+  }
+}
+else {
+  $post_args = array(
+  	'posts_per_page'   => -1,
+  	'post_type'        => 'post',
+  );
+}
+
+$the_query = new WP_Query( $post_args );
+
+// echo '<pre>';
+// var_dump($post_args);
+// echo '</pre>';
+?>
+<?php get_header(); ?>
+
 <!-- [ #container ] -->
 <div id="container" class="innerBox">
 
@@ -17,10 +57,17 @@ get_header(); ?>
     </section>
     <!-- [ /#search ] -->
 
-  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+  <?php if ( $the_query->have_posts() ) {
+
+    while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
     <?php get_template_part( 'includes/category', 'lawyer-panel' ); ?>
     <!-- .entry-content -->
-  <?php endwhile; ?>
+  <?php endwhile;
+  }
+  else {
+    echo 'No result';
+  }
+  ?>
 
   </section>
   <!-- [ /#content ] -->
