5aeaa8261afa1eefe787cebbbbe0c265a1e9ed74
julian-printemps <julian.printemps@gmail.com>
2016-02-04 11:27:02 +0900

    Search page info

--- wp/wp-content/themes/biz-vektor-child/search-info.php
+++ wp/wp-content/themes/biz-vektor-child/search-info.php
@@ -1,17 +1,66 @@
-<?php get_header();
 
-global $query_string;
-$post_category_slug = $_GET['info_cat'];
-// $taxonomy = 'info-cat';
-// $info_term = get_terms( $taxonomy, 'slug='.$post_category_slug);
+<?php
+$post_search = $_GET['s'];
+$post_type = $_GET['post_type'];
+$taxonomy = 'info-cat';
+$info_term = get_terms( $taxonomy, 'slug=kyoto');
+$info_term_id = $info_term[0]->term_id;
 
-$terms = get_the_terms( $post_category, 'info-cat' );
+$posts_array = get_posts(
+  array(
+    'posts_per_page' => -1,
+    'post_type' => 'info',
+    'tax_query' => array(
+      array(
+        'taxonomy' => $taxonomy,
+        'field' => 'term_id',
+        'terms' => $info_term_id,
+      )
+    )
+  )
+);
 
-query_posts( $query_string . '&category_name='.$info_term[0]->slug );
 
-echo $query_string . '&cat='.$info_term[0]->term_id;
+$post_search = $_GET['s'];
+$post_type = $_GET['post_type'];
+$post_slug = $_GET['info_cat'];
+$taxonomy = 'info-cat';
+$post_term = get_terms( $taxonomy, 'slug='.$post_slug);
+$post_term_id = $post_term[0]->term_id;
+$posts_array2 = get_posts(
+  array(
+    's' => $post_search,
+    'post_type' => $post_type,
+    'tax_query' => array(
+      array(
+        'taxonomy' => $taxonomy,
+        'field' => 'term_id',
+        'terms' => $post_term_id,
+      )
+    )
+  )
+);
 
+
+$post_args = array(
+    's' => $post_search,
+    'post_type' => $post_type,
+    'tax_query' => array(
+      array(
+        'taxonomy' => $taxonomy,
+        'field' => 'term_id',
+        'terms' => $post_term_id,
+      )
+    )
+  );
+
+$the_query = new WP_Query( $post_args );
+
+// echo '<pre>';
+// var_dump($posts_array2);
+// echo '</pre>';
 ?>
+<?php get_header(); ?>
 
 <!-- [ #container ] -->
 <div id="container" class="innerBox">
@@ -21,11 +70,11 @@ echo $query_string . '&cat='.$info_term[0]->term_id;
 
     <!-- [ #search ] -->
     <section class="searchArea">
-      <?php get_template_part( 'includes/search', 'info-search' ); ?>
+      <?php get_template_part( 'includes/search', 'info-form' ); ?>
     </section>
     <!-- [ /#search ] -->
 
-  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+  <?php if ( $the_query->have_posts() ) while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
     <?php get_template_part( 'includes/search', 'info-panel' ); ?>
     <!-- .entry-content -->
