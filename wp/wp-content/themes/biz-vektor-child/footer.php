ad42eb68a572d2188ebcc08a32a3408f250b642d
三光楼茉澄 <sankoro@neopa.jp>
2016-02-08 20:01:46 +0900

    update Facebook comment

--- wp/wp-content/themes/biz-vektor-child/footer.php
+++ wp/wp-content/themes/biz-vektor-child/footer.php
@@ -13,7 +13,9 @@
 
 <!-- [ #footerSection ] -->
 <div id="footerSection">
-<div class="fb-comments" data-href="http://et-support.handicap.jp" data-width="500" data-numposts="2"></div>	<div id="pagetop">
+<div class="fb-comments" data-href="http://et-support.handicap.jp/" data-width="600" data-numposts="5"></div>
+
+<div id="pagetop">
 	<div id="pagetopInner" class="innerBox">
 	<a href="#wrap">PAGETOP</a>
 	</div>
@@ -83,175 +85,175 @@
 }(document, 'script', 'facebook-jssdk'));</script>
 
 <script type="text/javascript">
-(function($) {
+	(function($) {
 
-/*
-*  new_map
-*
-*  This function will render a Google Map onto the selected jQuery element
-*
-*  @type	function
-*  @date	8/11/2013
-*  @since	4.3.0
-*
-*  @param	$el (jQuery element)
-*  @return	n/a
-*/
+	/*
+	*  new_map
+	*
+	*  This function will render a Google Map onto the selected jQuery element
+	*
+	*  @type	function
+	*  @date	8/11/2013
+	*  @since	4.3.0
+	*
+	*  @param	$el (jQuery element)
+	*  @return	n/a
+	*/
 
-function new_map( $el ) {
-	
-	// var
-	var $markers = $el.find('.marker');
-	
-	
-	// vars
-	var args = {
-		zoom		: 16,
-		center		: new google.maps.LatLng(0, 0),
-		mapTypeId	: google.maps.MapTypeId.ROADMAP
-	};
-	
-	
-	// create map	        	
-	var map = new google.maps.Map( $el[0], args);
-	
-	
-	// add a markers reference
-	map.markers = [];
-	
-	
-	// add markers
-	$markers.each(function(){
+	function new_map( $el ) {
 		
-    	add_marker( $(this), map );
+		// var
+		var $markers = $el.find('.marker');
 		
-	});
-	
-	
-	// center map
-	center_map( map );
-	
-	
-	// return
-	return map;
-	
-}
+		
+		// vars
+		var args = {
+			zoom		: 16,
+			center		: new google.maps.LatLng(0, 0),
+			mapTypeId	: google.maps.MapTypeId.ROADMAP
+		};
+		
+		
+		// create map	        	
+		var map = new google.maps.Map( $el[0], args);
+		
+		
+		// add a markers reference
+		map.markers = [];
+		
+		
+		// add markers
+		$markers.each(function(){
+			
+	    	add_marker( $(this), map );
+			
+		});
+		
+		
+		// center map
+		center_map( map );
+		
+		
+		// return
+		return map;
+		
+	}
 
-/*
-*  add_marker
-*
-*  This function will add a marker to the selected Google Map
-*
-*  @type	function
-*  @date	8/11/2013
-*  @since	4.3.0
-*
-*  @param	$marker (jQuery element)
-*  @param	map (Google Map object)
-*  @return	n/a
-*/
+	/*
+	*  add_marker
+	*
+	*  This function will add a marker to the selected Google Map
+	*
+	*  @type	function
+	*  @date	8/11/2013
+	*  @since	4.3.0
+	*
+	*  @param	$marker (jQuery element)
+	*  @param	map (Google Map object)
+	*  @return	n/a
+	*/
 
-function add_marker( $marker, map ) {
+	function add_marker( $marker, map ) {
 
-	// var
-	var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+		// var
+		var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
 
-	// create marker
-	var marker = new google.maps.Marker({
-		position	: latlng,
-		map			: map
-	});
-
-	// add to array
-	map.markers.push( marker );
-
-	// if marker contains HTML, add it to an infoWindow
-	if( $marker.html() )
-	{
-		// create info window
-		var infowindow = new google.maps.InfoWindow({
-			content		: $marker.html()
+		// create marker
+		var marker = new google.maps.Marker({
+			position	: latlng,
+			map			: map
 		});
 
-		// show info window when marker is clicked
-		google.maps.event.addListener(marker, 'click', function() {
+		// add to array
+		map.markers.push( marker );
 
-			infowindow.open( map, marker );
+		// if marker contains HTML, add it to an infoWindow
+		if( $marker.html() )
+		{
+			// create info window
+			var infowindow = new google.maps.InfoWindow({
+				content		: $marker.html()
+			});
+
+			// show info window when marker is clicked
+			google.maps.event.addListener(marker, 'click', function() {
+
+				infowindow.open( map, marker );
+
+			});
+		}
+
+	}
+
+	/*
+	*  center_map
+	*
+	*  This function will center the map, showing all markers attached to this map
+	*
+	*  @type	function
+	*  @date	8/11/2013
+	*  @since	4.3.0
+	*
+	*  @param	map (Google Map object)
+	*  @return	n/a
+	*/
+
+	function center_map( map ) {
+
+		// vars
+		var bounds = new google.maps.LatLngBounds();
+
+		// loop through all markers and create bounds
+		$.each( map.markers, function( i, marker ){
+
+			var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+
+			bounds.extend( latlng );
 
 		});
+
+		// only 1 marker?
+		if( map.markers.length == 1 )
+		{
+			// set center of map
+		    map.setCenter( bounds.getCenter() );
+		    map.setZoom( 16 );
+		}
+		else
+		{
+			// fit to bounds
+			map.fitBounds( bounds );
+		}
+
 	}
 
-}
+	/*
+	*  document ready
+	*
+	*  This function will render each map when the document is ready (page has loaded)
+	*
+	*  @type	function
+	*  @date	8/11/2013
+	*  @since	5.0.0
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+	// global var
+	var map = null;
 
-/*
-*  center_map
-*
-*  This function will center the map, showing all markers attached to this map
-*
-*  @type	function
-*  @date	8/11/2013
-*  @since	4.3.0
-*
-*  @param	map (Google Map object)
-*  @return	n/a
-*/
+	$(document).ready(function(){
 
-function center_map( map ) {
+		$('.acf-map').each(function(){
 
-	// vars
-	var bounds = new google.maps.LatLngBounds();
+			// create map
+			map = new_map( $(this) );
 
-	// loop through all markers and create bounds
-	$.each( map.markers, function( i, marker ){
-
-		var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
-
-		bounds.extend( latlng );
+		});
 
 	});
 
-	// only 1 marker?
-	if( map.markers.length == 1 )
-	{
-		// set center of map
-	    map.setCenter( bounds.getCenter() );
-	    map.setZoom( 16 );
-	}
-	else
-	{
-		// fit to bounds
-		map.fitBounds( bounds );
-	}
-
-}
-
-/*
-*  document ready
-*
-*  This function will render each map when the document is ready (page has loaded)
-*
-*  @type	function
-*  @date	8/11/2013
-*  @since	5.0.0
-*
-*  @param	n/a
-*  @return	n/a
-*/
-// global var
-var map = null;
-
-$(document).ready(function(){
-
-	$('.acf-map').each(function(){
-
-		// create map
-		map = new_map( $(this) );
-
-	});
-
-});
-
-})(jQuery);
+	})(jQuery);
 </script>
 
 </body>
