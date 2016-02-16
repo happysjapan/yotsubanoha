d8ded825bcb1b42577004744d695ba3c76083bf2
satoshi <satoshi.choreo@gmail.com>
2016-02-16 11:34:22 +0900

    cd

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+cd `dirname $0`
+
 rsync -avhz --rsh=ssh --delete --exclude=.sass-cache --exclude=sass --progress ./wp/wp-content/themes/biz-vektor-child/ happys:~/www/etsupporthandicap/wp-content/themes/biz-vektor-child
 
 rsync -avhz --rsh=ssh --delete --progress ./wp/wp-content/plugins/yotsubanoha/ happys:~/www/etsupporthandicap/wp-content/plugins/yotsubanoha
