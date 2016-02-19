fce3be434cd93146246fe7dd35d93d203b7fc0a5
三光楼茉澄 <sankoro@neopa.jp>
2016-02-19 14:03:18 +0900

    Adding auto input lawyer name in form

--- wp/wp-content/themes/biz-vektor-child/includes/category-lawyer-panel.php
+++ wp/wp-content/themes/biz-vektor-child/includes/category-lawyer-panel.php
@@ -50,8 +50,8 @@
         </div>
         <div class="btnWrap">
           <a href="tel:0120-697-182" title="無料電話相談" class="btnLightGreen" onclick="ga('send', 'event', '電話リンク', 'タップ', '一覧ボタン');">無料電話相談</a>
-          <a href="/contact-form/" title="資料請求" class="btnLightBlue">資料請求</a>
-          <a href="/tour-form/" title="見学会へ参加" class="btnBlue">見学会へ参加</a>
+          <a href="/contact-form/?office=<?php the_title(); ?>" title="資料請求" class="btnLightBlue">資料請求</a>
+          <a href="/tour-form/?office=<?php the_title(); ?>" title="見学会へ参加" class="btnBlue">見学会へ参加</a>
         </div>
       </div>
       <a href="<?php the_permalink(); ?>"  title="詳細ページへ" class="linkToDetail">▶︎&nbsp;詳細ページへ</a>
