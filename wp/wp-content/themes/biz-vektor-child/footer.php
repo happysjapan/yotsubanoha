8a3b23202f30852495aeea882df481ffabc830ca
julian-printemps <julian.printemps@gmail.com>
2016-05-06 14:54:34 +0800

    Update plugins

--- wp/wp-content/themes/biz-vektor-child/footer.php
+++ wp/wp-content/themes/biz-vektor-child/footer.php
@@ -92,42 +92,42 @@
 	*/
 
 	function new_map( $el ) {
-		
+
 		// var
 		var $markers = $el.find('.marker');
-		
-		
+
+
 		// vars
 		var args = {
 			zoom		: 16,
 			center		: new google.maps.LatLng(0, 0),
 			mapTypeId	: google.maps.MapTypeId.ROADMAP
 		};
-		
-		
-		// create map	        	
+
+
+		// create map
 		var map = new google.maps.Map( $el[0], args);
-		
-		
+
+
 		// add a markers reference
 		map.markers = [];
-		
-		
+
+
 		// add markers
 		$markers.each(function(){
-			
+
 	    	add_marker( $(this), map );
-			
+
 		});
-		
-		
+
+
 		// center map
 		center_map( map );
-		
-		
+
+
 		// return
 		return map;
-		
+
 	}
 
 	/*
